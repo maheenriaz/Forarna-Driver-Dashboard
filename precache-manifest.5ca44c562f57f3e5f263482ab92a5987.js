b8f880471f95e6732715f9e364d678e082b5774a
maheenriaz <mahiriaz99@gmail.com>
2021-03-05 21:30:04 -0800

    Updates

---
+++ precache-manifest.5ca44c562f57f3e5f263482ab92a5987.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ee64912a786307cbe978f6d40a64d8",
+    "url": "/Forarna-Driver-Dashboard/index.html"
+  },
+  {
+    "revision": "a478c61aa25d19d25eb9",
+    "url": "/Forarna-Driver-Dashboard/static/css/2.7da3e5d4.chunk.css"
+  },
+  {
+    "revision": "c33a4d1e894a291223e1",
+    "url": "/Forarna-Driver-Dashboard/static/css/main.be5f5bea.chunk.css"
+  },
+  {
+    "revision": "a478c61aa25d19d25eb9",
+    "url": "/Forarna-Driver-Dashboard/static/js/2.4251b462.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Forarna-Driver-Dashboard/static/js/2.4251b462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33a4d1e894a291223e1",
+    "url": "/Forarna-Driver-Dashboard/static/js/main.37cec19f.chunk.js"
+  },
+  {
+    "revision": "ee63680918afe09f2a66",
+    "url": "/Forarna-Driver-Dashboard/static/js/runtime-main.79176091.js"
+  },
+  {
+    "revision": "1781e57de7f9e299ce8b94e65cc1f921",
+    "url": "/Forarna-Driver-Dashboard/static/media/cartoon.1781e57d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Forarna-Driver-Dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
